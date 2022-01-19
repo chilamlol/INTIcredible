38fb078ff17c669e6de806e85d43ef2d847d9e91
Lucas Chan <chilamlol@hotmail.com>
2022-01-19 19:13:42 +0800

    Changed db_config to pythonanywhere address from localhost

--- db_config.py
+++ db_config.py
@@ -4,10 +4,20 @@ from flaskext.mysql import MySQL
 mysql = MySQL()
 
 # MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'chilamlol'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'intidemo321'
+app.config['MYSQL_DATABASE_DB'] = 'chilamlol$alumni'
+app.config['MYSQL_DATABASE_HOST'] = 'chilamlol.mysql.pythonanywhere-services.com'
+
+"""
+MySQL configurations for localhost
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'alumni'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+"""
+
+
 
 mysql.init_app(app)
 #intidemo321
