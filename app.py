2597e62bee66e3f7fa6aaf51218d2933f59004dd
Lucas Chan <chilamlol@hotmail.com>
2022-01-25 20:15:24 +0800

    Added secret key for encryption

--- app.py
+++ app.py
@@ -2,4 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '\x12\xc9\xf7<?s5\xed\x17\xd4M\xa8\xdc3\x01\xe8\xfa\x8e\x10]\x07YA\x8b'
+
 CORS(app)
