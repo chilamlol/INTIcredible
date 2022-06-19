3d0cb05b42777dbdaf340fc4475f0d89f014f491
Lucas Chan <chilamlol@hotmail.com>
2022-06-20 02:25:05 +0800

    added machine learning on event

--- controller/event_controller.py
+++ controller/event_controller.py
@@ -6,9 +6,24 @@ from datetime import datetime
 import json
 from token_verifier import *
 
+# recommender system
+import pandas as pd
+from pandas import json_normalize
+import math
+from sklearn.model_selection import train_test_split
+from database.db_config import mysql
+
+# dummy data
+import random
+
 
 def convertStringToDateTime(str):
     return datetime.strptime(str, '%Y-%m-%d %H:%M:%S')
+
+
+# log transformation to smooth the distribution
+def smooth_user_preference(x):
+    return math.log(1+x, 2)
 
 
 # add events
@@ -179,6 +194,133 @@ def delete_event(eventId):
             return resp
 
         return not_found
+    except Exception as e:
+        print(e)
+        return internal_server_error(e)
+
+
+# Event recommender
+# insert dummy data
+@app.route('/event/generate-dummy/<int:amount>')
+def generate_dummy_event(amount):
+
+    sql = " SELECT DISTINCT userId FROM tbl_user "
+    userIdJson = readAllRecord(sql)
+
+    # Declare empty userId list
+    userIdList = []
+
+    for userId in userIdJson:
+        userIdList.append(userId['userId'])
+
+    sql = " SELECT DISTINCT eventId FROM tbl_event"
+    eventIdJson = readAllRecord(sql)
+
+    # Declare empty eventId list
+    eventIdList = []
+
+    for eventId in eventIdJson:
+        eventIdList.append(eventId['eventId'])
+
+    sql = " INSERT INTO tbl_event_activities (timestamp, userId, eventId, eventType) VALUES (%s, %s, %s, %s)"
+
+    # Getting the current date and time
+    dt = datetime.now()
+
+    # getting the timestamp
+    ts = datetime.timestamp(dt)
+
+    # Insert to table
+    for i in range(amount):
+        data = (int(ts), random.choice(userIdList), random.choice(eventIdList), random.choice(['VIEW', 'JOIN']))
+
+        createRecord(sql, data)
+
+    resp = jsonify(message='Dummy data created')
+    return resp
+
+
+# Work in Progress
+# Event recommender
+@app.route('/event/train-model')
+def get_popular_events():
+    try:
+        # Run SQL
+        sql = "SELECT timestamp, eventType, eventId, userId FROM tbl_event_activities"
+        rows = readAllRecord(sql)
+
+        # Loads json into dataframe
+        interactions_df = json_normalize(rows)
+
+        # define event strength
+        event_type_strength = {
+            'VIEW': 1.0,
+            'JOIN': 3.0,
+        }
+
+        interactions_df['eventStrength'] = interactions_df['eventType'].apply(lambda x: event_type_strength[x])
+
+        # Recommender systems have a problem known as user cold-start,
+        # in which is hard do provide personalized recommendations for users with none or a very few number of consumed items,
+        # due to the lack of information to model their preferences.
+
+        # For this reason, we are keeping in the dataset only users with at leas 5 interactions.
+
+        # Get total users
+        users_interactions_count_df = interactions_df.groupby(['userId', 'eventId']).size().groupby('userId').size()
+
+        # Get user with at least 5 interactions
+        users_with_enough_interactions_df = users_interactions_count_df[users_interactions_count_df >= 5].reset_index()[['userId']]
+
+        # Get total interactions of users with at least 5 interactions
+        interactions_from_selected_users_df = interactions_df.merge(users_with_enough_interactions_df,
+                                                                    how='right',
+                                                                    left_on='userId',
+                                                                    right_on='userId')
+
+        # Get unique user/item interactions
+        interactions_full_df = interactions_from_selected_users_df \
+            .groupby(['userId', 'eventId'])['eventStrength'].sum() \
+            .apply(smooth_user_preference).reset_index()
+
+        # Train the model, the ratio is 80:20 (train:test)
+        interactions_train_df, interactions_test_df = train_test_split(interactions_full_df,
+                                                                       stratify=interactions_full_df['userId'],
+                                                                       test_size=0.20,
+                                                                       random_state=42)
+
+        # Indexing by userId to speed up the searches during evaluation
+        # interactions_full_indexed_df = interactions_full_df.set_index('userId')
+        # interactions_train_indexed_df = interactions_train_df.set_index('userId')
+        # interactions_test_indexed_df = interactions_test_df.set_index('userId')
+
+        print('# interactions on Train set: %d' % len(interactions_train_df))
+        print('# interactions on Test set: %d' % len(interactions_test_df))
+
+        # Computes the most popular items
+        item_popularity_df = interactions_full_df.groupby('eventId')['eventStrength'].sum().sort_values(
+            ascending=False).reset_index()
+        print(item_popularity_df)
+
+        conn = mysql.connect()
+        cursor = conn.cursor()
+
+        # creating column list for insertion
+        cols = "`,`".join([str(i) for i in item_popularity_df.columns.tolist()])
+
+        for i, row in item_popularity_df.iterrows():
+            sql = "INSERT INTO `tbl_event_strength` (`" + cols + "`, createdDate) VALUES (" + "%s," * (len(row) - 1) + "%s, NOW())"
+            cursor.execute(sql, tuple(row))
+
+            # the connection is not autocommitted by default, so we must commit to save our changes
+            conn.commit()
+
+        # close connection
+        conn.close()
+
+        resp = jsonify(message="Event strength has been updated")
+        return resp
+
     except Exception as e:
         print(e)
         return internal_server_error(e)
