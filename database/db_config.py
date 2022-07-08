5f07fba74ae280b2af6202c9851580a9d8c8ab70
Lucas Chan <chilamlol@hotmail.com>
2022-07-08 18:16:29 +0800

    Update db_config.py

--- database/db_config.py
+++ database/db_config.py
@@ -6,7 +6,7 @@ mysql = MySQL()
 """
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'chilamlol'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'intidemo321'
+app.config['MYSQL_DATABASE_PASSWORD'] = '*****'
 app.config['MYSQL_DATABASE_DB'] = 'chilamlol$alumni'
 app.config['MYSQL_DATABASE_HOST'] = 'chilamlol.mysql.pythonanywhere-services.com'
 
